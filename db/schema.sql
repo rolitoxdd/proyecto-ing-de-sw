c8a8dee2ddb67f93eb16bb8b4043240749f95cc6
rolitoxdd <rolo.alv97@gmail.com>
2022-04-17 21:56:41 -0400

    aaaaaaaaaaaaaaaaaaaaaaaa

--- db/schema.sql
+++
@@ -1,8 +0,0 @@
-create database test;
-use test;
-create table Products (
-  id serial not null,
-  name varchar(30) not null,
-  price varchar(30) not null,
-  stock int not null
-);
--- pages/list.jsx
+++ pages/list.jsx
@@ -1,40 +1,43 @@
 import { useEffect, useState } from 'react';
 import { ListGroupItem, ListGroup, CardGroup } from 'reactstrap';
 import ProductCard from '../components/ProductCard';
-import { Card, CardImg, CardBody, CardTitle, CardSubtitle, Button, Row, Col } from 'reactstrap';
+import { Card, CardImg, CardBody, CardTitle, CardSubtitle, Button, Row, Col, CardColumns } from 'reactstrap';
 
 export default function List() {
-  const [products, setProducts] = useState([]);
+  const [products, setProducts] = useState([]); // name, img, price, details, stock
+  let htmlDeProductos = [];
   useEffect(async () => {
     const res = await fetch('/api/products');
     const data = await res.json();
     setProducts(data);
   }, []);
+  for (let prod of products) {
+    htmlDeProductos.push(<li style={{ background: 'red', color: 'white' }}>{prod.name}</li>);
+  }
   return (
     <>
       <div className="next-steps my-5" data-testid="content">
-        <ListGroup>
+        {/* <ul>{htmlDeProductos}</ul> */}
+        {/* <CardColumns>
           {products.map(({ name, img, price }, index) => (
-            <ListGroupItem>
-              <Card key={name}>
-                <Row className="no-gutters">
-                  <Col className="md-2">
-                    <CardImg alt={name} src={img} top width="100%" />
-                  </Col>
-                  <Col className="md-10">
-                    <CardBody>
-                      <CardTitle tag="h5">{name}</CardTitle>
-                      <CardSubtitle className="mb-2 text-muted" tag="h6">
-                        {price}
-                      </CardSubtitle>
-                      <Button>Button</Button>
-                    </CardBody>
-                  </Col>
-                </Row>
-              </Card>
-            </ListGroupItem>
+            <Card key={name}>
+              <Row className="no-gutters">
+                <Col className="md-2">
+                  <CardImg alt={name} src={img} top width="100%" />
+                </Col>
+                <Col className="md-10">
+                  <CardBody>
+                    <CardTitle tag="h5">{name}</CardTitle>
+                    <CardSubtitle className="mb-2 text-muted" tag="h6">
+                      {price}
+                    </CardSubtitle>
+                    <Button>Button</Button>
+                  </CardBody>
+                </Col>
+              </Row>
+            </Card>
           ))}
-        </ListGroup>
+        </CardColumns> */}
       </div>
     </>
   );
